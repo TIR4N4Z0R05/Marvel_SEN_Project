aa809c531650406063e981ee10cce1a37802e704
TIR4N4Z0R05 <mhkazemi_engineer@yahoo.com>
2016-08-16 15:34:43 +0430

    Modifying Get2d map

--- src/ReadMap.cpp
+++ src/ReadMap.cpp
@@ -1,5 +1,10 @@
 #include "ReadMap.h"
 
+using namespace mrpt;
+using namespace mrpt::utils;
+using namespace mrpt::math;
+using namespace mrpt::random;
+using namespace mrpt::poses;
 using namespace std;
 
 void getmapdata (const sensor_msgs::PointCloud2::ConstPtr&input){
@@ -8,12 +13,22 @@ void getmapdata (const sensor_msgs::PointCloud2::ConstPtr&input){
     pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
 
-    clear_grid();
+    cout << "gotit" << endl;
+
+    if(z-z0>0.3){
+	//cout << "clearing" << endl;
+	z0 = z;
+    	clear_grid();
+    }
+    lines.resize(0);
 
     for(unsigned int i=0;i<temp_cloud->points.size();i++){
-	if(temp_cloud->points.at(i).z > z-0.025 && temp_cloud->points.at(i).z < z+0.025){
+	if(temp_cloud->points.at(i).z > z-0.2 && temp_cloud->points.at(i).z < z+0.2){
 		grid_x = temp_cloud->points.at(i).x/PP_grid.info.resolution + PP_grid.info.width/2;
 		grid_y = temp_cloud->points.at(i).y/PP_grid.info.resolution + PP_grid.info.height/2;
+
+		xs.push_back(temp_cloud->points.at(i).x);
+		ys.push_back(temp_cloud->points.at(i).y);
 
 		/*if(grid_x<0 || grid_x>=PP_grid.info.width){
 			PP_grid.info.width = PP_grid.info.width + abs(grid_x - PP_grid.info.width);
@@ -27,8 +42,10 @@ void getmapdata (const sensor_msgs::PointCloud2::ConstPtr&input){
 		}*/
 		
 		PP_grid.data.at( (grid_y-1)*PP_grid.info.width + grid_x ) = 100;
+		
 	}
     }
+    //ransac_filter();
     draw();
     pub.publish(PP_grid);
     ros::spinOnce();
@@ -38,7 +55,9 @@ void getmapdata (const sensor_msgs::PointCloud2::ConstPtr&input){
 void chatterCallback(const nav_msgs::Odometry::ConstPtr&msg)
 {
 	z=msg->pose.pose.position.z;
-	cout << z << endl;
+	p.x = msg->pose.pose.position.x; p.y = msg->pose.pose.position.y; p.z = msg->pose.pose.position.z;
+	if(movement(p) > 1)
+		trajectory.push_back(p);
 }
 
 int main (int argc, char **argv){
@@ -50,11 +69,15 @@ int main (int argc, char **argv){
    glutCreateWindow("Map_View");
    glOrtho(0,640,640,0,-1,1);
 //nodehandle stuff/*****************************************************************************
+
+tr_init();
+z0 = 0;
+
 licence();
 grid_init();
 ros::init(argc, argv, "ReadMaP");
 ros::NodeHandle n;
-ros::Subscriber sub = n.subscribe("/rtabmap/cloud_map", 1000, getmapdata);
+ros::Subscriber sub = n.subscribe("/rtabmap/octomap_cloud", 1000, getmapdata);
 ros::Subscriber sub1 = n.subscribe("/rtabmap/odom", 1000, chatterCallback);
 pub = n.advertise<nav_msgs::OccupancyGrid>("new_map", 1000);
 ros::spin();
@@ -97,12 +120,57 @@ void draw(){
 	}
 
 	glEnd();
+	
   	glFlush();
 }
 
 void clear_grid(){
-	for(unsigned int i=0 ; i<PP_grid.data.size() ; i++){
-		PP_grid.data.at(i) = -1;	
+	int tr_x, tr_y;
+	//for(unsigned int i=0 ; i<trajectory.size(); i++){
+	//	tr_x = trajectory.at(i).x/PP_grid.info.resolution + PP_grid.info.width/2;
+	//	tr_y = trajectory.at(i).y/PP_grid.info.resolution + PP_grid.info.height/2;	
+		
+		for(unsigned int j=0; j< PP_grid.data.size(); j++){
+			if(/*grid_dis(tr_x,tr_y,j/PP_grid.info.width,j%PP_grid.info.width) < 20 &&*/ PP_grid.data.at(j) == 100)
+				PP_grid.data.at(j) == -1;		
+		}
+		
+	//}
+}
+
+void ransac_filter(){
+	TPoint2D p1, p2;
+	vector<pair<size_t,TLine2D > > detectedLines;
+	const double DIST_THRESHOLD = 0.1;
+
+	ransac_detect_2D_lines(xs,ys,detectedLines,DIST_THRESHOLD,100 );
+
+	for (vector<pair<size_t,TLine2D> >::iterator p=detectedLines.begin();p!=detectedLines.end();++p)
+	{
+		CVectorDouble lx(2),ly(2);
+		lx[0] = -1;
+		lx[1] = 1;
+		for (CVectorDouble::Index q=0;q<lx.size();q++){
+			ly[q] = -(p->second.coefs[2]+p->second.coefs[0]*lx[q])/p->second.coefs[1];
+			//cout << p->first
+		}
+		grid_x = lx[0]/PP_grid.info.resolution + PP_grid.info.width/2;
+		grid_y = ly[0]/PP_grid.info.resolution + PP_grid.info.height/2;
+		grid_x1 = lx[1]/PP_grid.info.resolution + PP_grid.info.width/2;
+		grid_y1 = ly[1]/PP_grid.info.resolution + PP_grid.info.height/2;
+		lines.push_back({grid_x, grid_x1, grid_y, grid_y1});
 	}
 }
 
+double movement(pcl::PointXYZ curr){
+	return sqrt(pow(trajectory.back().x-curr.x,2) + pow(trajectory.back().y-curr.y,2));
+}
+
+void tr_init(){
+	pcl::PointXYZ p0; p0.x = 0; p0.y = 0; p0.z = 0;
+	trajectory.push_back(p0);
+}
+
+double grid_dis(int x0, int y0, int x1, int y1){
+	return sqrt(pow(x1-x0,2) + pow(y1-y0,2));
+}
--- src/ReadMap.h
+++ src/ReadMap.h
@@ -1,3 +1,11 @@
+#include <mrpt/math/ransac_applications.h>
+#include <mrpt/random.h>
+#include <mrpt/utils/metaprogramming.h>
+#include <mrpt/poses/CPose3D.h>
+#include <mrpt/opengl/CGridPlaneXY.h>
+#include <mrpt/opengl/CPointCloud.h>
+#include <mrpt/opengl/stock_objects.h>
+#include <mrpt/opengl/CTexturedPlane.h>
 #include "ros/ros.h"
 #include <math.h>
 #include <vector>
@@ -20,14 +28,42 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include "nav_msgs/Odometry.h"
 #include <GL/glut.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+
+using namespace mrpt;
+using namespace mrpt::utils;
+using namespace mrpt::math;
+using namespace mrpt::random;
+using namespace mrpt::poses;
 using namespace std;
-float z;
-int grid_x, grid_y, grid_z;
+
+struct line{
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+};
+
+
+float z, z0;
+int grid_x, grid_y, grid_x1, grid_y1;
 vector <pcl::PointXYZ> grid_points;
+CVectorDouble xs,ys;
+CVectorDouble lxs,lys;
 nav_msgs::OccupancyGrid PP_grid;
 ros::Publisher pub;
+vector <line> lines;
+vector <pcl::PointXYZ> trajectory;
+pcl::PointXYZ p;
 
+double movement(pcl::PointXYZ);
 void grid_init();
 void draw();
 void clear_grid();
+void ransac_filter();
+void registerOnGrid(float x1, float x2, float y1, float y2);
+void tr_init();
+double grid_dis(int ,int ,int ,int);
